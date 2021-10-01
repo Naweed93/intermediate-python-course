899a115230b3797883ab6bc7dfb6aac4a3b8c61a
Naweed <iamnavid93@gmail.com>
2021-10-01 18:49:09 +0330

    asking user for number of dices and sides

--- dice_roller.py
+++ dice_roller.py
@@ -1,10 +1,16 @@
 import random
 def main():
-    rolldice = 2
+    rolldice = int(input("how many times do you need to roll a dice? "))
+    diceSide = int(input("and how many side that dice has? "))
     sum = 0
     for i in range(0,rolldice):
-        rnd = random.randint(1,6)
-        print(f'You rolled a {rnd}')
+        rnd = random.randint(1,diceSide)
+        if rnd == 1:
+            print(f'you rolled {rnd}, better luck next time')
+        elif rnd == diceSide:
+            print(f'BINGO, you rolled {rnd}')
+        else:
+            print(f'You rolled a {rnd}')
         sum += rnd
     print(f'total dice number is {sum}')
 
