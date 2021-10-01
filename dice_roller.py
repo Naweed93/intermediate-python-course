ca69af5e6745c191cf2aafed6beff1c1d50bb597
Naweed <iamnavid93@gmail.com>
2021-10-01 18:39:41 +0330

    random option added to dices

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,12 @@
+import random
 def main():
-  print('You rolled a die')
+    rolldice = 2
+    sum = 0
+    for i in range(0,rolldice):
+        rnd = random.randint(1,6)
+        print(f'You rolled a {rnd}')
+        sum += rnd
+    print(f'total dice number is {sum}')
 
 if __name__== "__main__":
   main()
